84b490703fad64076c8a3db86691e435cf267f73
Khomenko <itm.ikhomenko@gmail.com>
2021-02-24 14:50:33 +0200

    Change to My name

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,6 @@ package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Hello GoIT world");
-      System.out.println("This is second commit");
+      System.out.println("Hello! My name is Inna");
     }
 }
